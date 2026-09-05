5ca205cf77ba8da6f679e4b15b06e5780be0072a
Rojesh Niroula <wonderingmind2005@gmail.com>
2026-09-05 19:22:39 +0545

    This is my lab 11

---
+++ Lab_Report_11/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Lab_Report_11.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lab_Report_11.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ Lab_Report_11/Models/Student.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lab_Report_11.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Course { get; set; }
+
+        public string Batch { get; set; }
+
+        public int JoinedYear { get; set; }
+    }
+}

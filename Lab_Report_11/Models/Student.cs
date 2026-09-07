8bc37409e145fd3becde1f837e128be9926ffc3b
sumit.neupane001 <sumit.neupane2062@gmail.com>
2026-09-07 08:38:04 +0545

    This is lab 11

---
+++ Lab_Report_11/Models/Student.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lab11.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Course { get; set; }
+
+        public string Batch { get; set; }
+
+        [Display(Name = "Joined Year")]
+        public int JoinedYear { get; set; }
+    }
+}

02a42de53cfa9222b99f32a72b2b9f1317a7048b
samriddha77 <poudelsamriddha77@gmail.com>
2026-09-08 11:39:28 +0545

    lab11

---
+++ Lab_Report_11/Models/Student.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lab_Report_11.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(1, 100)]
+        public int Age { get; set; }
+
+        [Required]
+        public string Phone { get; set; }
+
+        [Required]
+        public string Course { get; set; }
+
+        [Required]
+        public string Batch { get; set; }
+
+        [Required]
+        [Range(1900, 2100)]
+        public int JoinedYear { get; set; }
+    }
+}

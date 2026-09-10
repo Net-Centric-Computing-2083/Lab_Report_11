b25e747a56d650d6fe8de88ace57c924f9a334b3
Samikshya Saud <12345678+SamikshyaSaud@users.noreply.github.com>
2026-09-10 10:47:54 +0545

    Add Lab Report 11

---
+++ Models/Student.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab_Report_11.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        public int Age { get; set; }
+
+        public string Phone { get; set; } = string.Empty;
+
+        public string Course { get; set; } = string.Empty;
+
+        public string Batch { get; set; } = string.Empty;
+
+        public int JoinedYear { get; set; }
+    }
+}
